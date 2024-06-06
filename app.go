0aa4e984bd08a157790e3f32eb2ee59e02826d2d
soniharshil54 <sharshil43@yahoo.com>
2024-06-06 15:31:00 +0530

    user input for all fields

--- app.go
+++ app.go
@@ -8,10 +8,17 @@ import (
 func main() {
 	const inflationRate = 2.5
 	var investmentAmount float64
-	expectedReturnRate := 5.5
-	years := 10.0
+	var years float64
+	var expectedReturnRate float64
 
+	fmt.Print("Enter the investment amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter the number of years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Enter the expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
