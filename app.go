55cf35b7f9259caf040b9441034da3e318258893
soniharshil54 <sharshil43@yahoo.com>
2024-06-07 12:16:34 +0530

    function calculateFutureValues added

--- app.go
+++ app.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
 	var investmentAmount float64
 	var years float64
 	var expectedReturnRate float64
@@ -20,9 +21,14 @@ func main() {
 	fmt.Print("Enter the expected return rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, years, expectedReturnRate)
 
 	fmt.Println("futureValue Amount: ", futureValue)
 	fmt.Println("futureRealValue Amount: ", futureRealValue)
+}
+
+func calculateFutureValues(investmentAmount float64, years float64, expectedReturnRate float64) (float64, float64) {
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	return futureValue, futureRealValue
 }
